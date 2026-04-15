db3fc739f650b90e071a3497312100cdd596d6cd
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-04-15 08:28:24 +0200

    feat: :zap: ADD: robots.txt y actualizacion del sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,19 +8,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1, // La joya de la corona
+      priority: 1, 
+    },
+    {
+      url: `${baseUrl}/cetes-vs-sofipos`, // Nueva ruta
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9, 
+    },
+    {
+      url: `${baseUrl}/guia-afore`, // Nueva ruta
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9, 
     },
     {
       url: `${baseUrl}/guia`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8, // Contenido de valor para SEO
+      priority: 0.8, 
     },
     {
       url: `${baseUrl}/privacidad`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 0.2, // Importante legalmente, pero no para atraer tráfico
+      priority: 0.2, 
     },
     {
       url: `${baseUrl}/contacto`,
